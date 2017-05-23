62d942adc67738009d27faacc8536e421ee5091c
Richard <weizhou_hb@163.com>
2017-05-23 20:59:23 +0800

    代码格式标准化

--- APP/Send_Data/SendData.c
+++ APP/Send_Data/SendData.c
@@ -2,15 +2,15 @@
 
 
 static u8 buf[24];//DMA发送静态缓存数组
-void SCI_DIS(u32 ch0,u32 ch1,u32 ch2)//显示数据范围为-4000~+4000
+void SCI_DIS(u32 ch0, u32 ch1, u32 ch2) //显示数据范围为-4000~+4000
 {
-    buf[0]=((ch0+4000)/90+' ');
-    buf[1]=((ch0+4000)%90+' ');
-    buf[2]=((ch1+4000)/90+' ');
-    buf[3]=((ch1+4000)%90+' ');
-    buf[4]=((ch2+4000)/90+' ');
-    buf[5]=((ch2+4000)%90+' ');
-		DMA1_USART1_SEND((u32)buf,6);
+	buf[0] = ((ch0 + 4000) / 90 + ' ');
+	buf[1] = ((ch0 + 4000) % 90 + ' ');
+	buf[2] = ((ch1 + 4000) / 90 + ' ');
+	buf[3] = ((ch1 + 4000) % 90 + ' ');
+	buf[4] = ((ch2 + 4000) / 90 + ' ');
+	buf[5] = ((ch2 + 4000) % 90 + ' ');
+	DMA1_USART1_SEND((u32)buf, 6);
 }
 
 
@@ -28,149 +28,149 @@ void SCI_DIS(u32 ch0,u32 ch1,u32 ch2)//显示数据范围为-4000~+4000
 		s16 tempr 温度 。 单位0.1摄氏度   范围：直到你的电路板不能正常工作
 		s16 press 气压压力。单位10Pa  一个大气压强在101300pa 这个已经超过一个整型的范围。需要除以10再发给上位机
 		s16 IMUpersec  姿态解算速率。运算IMUpersec每秒。
-输出参数：没有	
+输出参数：没有
 *******************************************************************************/
-void DMA_UART1_ReportIMU(s16 yaw,s16 pitch,s16 roll,s16 alt,s16 tempr,s16 press,s16 IMUpersec)
+void DMA_UART1_ReportIMU(s16 yaw, s16 pitch, s16 roll, s16 alt, s16 tempr, s16 press, s16 IMUpersec)
 {
- 	unsigned int temp=0xaF+2;
+	unsigned int temp = 0xaF + 2;
 	static char ctemp;
 	//USART_SendData(USART1,0xa5);
-  //USART_SendData(USART1,0x5a);
-  //USART_SendData(USART1,14+2);
+	//USART_SendData(USART1,0x5a);
+	//USART_SendData(USART1,14+2);
 	//USART_SendData(USART1,0xA1);
-	buf[0]=0xA5;
-	buf[1]=0x5a;
-	buf[2]=16;
-	buf[3]=0xa1;
+	buf[0] = 0xA5;
+	buf[1] = 0x5a;
+	buf[2] = 16;
+	buf[3] = 0xa1;
 
 
-	if(yaw<0)yaw=32768-yaw;
-	ctemp=yaw>>8;
-  //USART_SendData(USART1,ctemp);
-	buf[4]=ctemp;
-	temp+=ctemp;
-	ctemp=yaw;
-  //USART_SendData(USART1,ctemp);
-	buf[5]=ctemp;
-	temp+=ctemp;
-
-	if(pitch<0)pitch=32768-pitch;
-	ctemp=pitch>>8;
+	if (yaw < 0)yaw = 32768 - yaw;
+	ctemp = yaw >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[6]=ctemp;
-	temp+=ctemp;
-	ctemp=pitch;
+	buf[4] = ctemp;
+	temp += ctemp;
+	ctemp = yaw;
+	//USART_SendData(USART1,ctemp);
+	buf[5] = ctemp;
+	temp += ctemp;
+
+	if (pitch < 0)pitch = 32768 - pitch;
+	ctemp = pitch >> 8;
+	//USART_SendData(USART1,ctemp);
+	buf[6] = ctemp;
+	temp += ctemp;
+	ctemp = pitch;
 	///USART_SendData(USART1,ctemp);
-	buf[7]=ctemp;
-	temp+=ctemp;
+	buf[7] = ctemp;
+	temp += ctemp;
 
-	if(roll<0)roll=32768-roll;
-	ctemp=roll>>8;
+	if (roll < 0)roll = 32768 - roll;
+	ctemp = roll >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[8]=ctemp;
-	temp+=ctemp;
-	ctemp=roll;
+	buf[8] = ctemp;
+	temp += ctemp;
+	ctemp = roll;
 	//USART_SendData(USART1,ctemp);
-	buf[9]=ctemp;
-	temp+=ctemp;
+	buf[9] = ctemp;
+	temp += ctemp;
 
-	if(alt<0)alt=32768-alt;
-	ctemp=alt>>8;
+	if (alt < 0)alt = 32768 - alt;
+	ctemp = alt >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[10]=ctemp;
-	temp+=ctemp;
-	ctemp=alt;
+	buf[10] = ctemp;
+	temp += ctemp;
+	ctemp = alt;
 	//USART_SendData(USART1,ctemp);
-	buf[11]=ctemp;
-	temp+=ctemp;
+	buf[11] = ctemp;
+	temp += ctemp;
 
-	if(tempr<0)tempr=32768-tempr;
-	ctemp=tempr>>8;
+	if (tempr < 0)tempr = 32768 - tempr;
+	ctemp = tempr >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[12]=ctemp;
-	temp+=ctemp;
-	ctemp=tempr;
+	buf[12] = ctemp;
+	temp += ctemp;
+	ctemp = tempr;
 	//USART_SendData(USART1,ctemp);
-	buf[13]=ctemp;
-	temp+=ctemp;
+	buf[13] = ctemp;
+	temp += ctemp;
 
-	if(press<0)press=32768-press;
-	ctemp=press>>8;
+	if (press < 0)press = 32768 - press;
+	ctemp = press >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[14]=ctemp;
-	temp+=ctemp;
-	ctemp=press;
+	buf[14] = ctemp;
+	temp += ctemp;
+	ctemp = press;
 	//USART_SendData(USART1,ctemp);
-	buf[15]=ctemp;
-	temp+=ctemp;
+	buf[15] = ctemp;
+	temp += ctemp;
 
 	//USART_SendData(USART1,temp%256);
 	//USART_SendData(USART1,0xaa);
-  buf[16]=temp%256;
-	buf[17]=0xaa;
-	DMA1_USART1_SEND((u32)buf,18);
+	buf[16] = temp % 256;
+	buf[17] = 0xaa;
+	DMA1_USART1_SEND((u32)buf, 18);
 }
 
 
-void UART1_ReportIMU(s16 yaw,s16 pitch,s16 roll,s16 alt,s16 tempr,s16 press,s16 IMUpersec)
+void UART1_ReportIMU(s16 yaw, s16 pitch, s16 roll, s16 alt, s16 tempr, s16 press, s16 IMUpersec)
 {
- 	unsigned int temp=0xaF+2;
+	unsigned int temp = 0xaF + 2;
 	static char ctemp;
-	USART_SendData(USART1,0xa5);
-  USART_SendData(USART1,0x5a);
-  USART_SendData(USART1,14+2);
-	USART_SendData(USART1,0xA1);
+	USART_SendData(USART1, 0xa5);
+	USART_SendData(USART1, 0x5a);
+	USART_SendData(USART1, 14 + 2);
+	USART_SendData(USART1, 0xA1);
 
-	if(yaw<0)yaw=32768-yaw;
-	ctemp=yaw>>8;
-  USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=yaw;
-  USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (yaw < 0)yaw = 32768 - yaw;
+	ctemp = yaw >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = yaw;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(pitch<0)pitch=32768-pitch;
-	ctemp=pitch>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=pitch;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (pitch < 0)pitch = 32768 - pitch;
+	ctemp = pitch >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = pitch;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(roll<0)roll=32768-roll;
-	ctemp=roll>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=roll;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (roll < 0)roll = 32768 - roll;
+	ctemp = roll >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = roll;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(alt<0)alt=32768-alt;
-	ctemp=alt>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=alt;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (alt < 0)alt = 32768 - alt;
+	ctemp = alt >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = alt;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(tempr<0)tempr=32768-tempr;
-	ctemp=tempr>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=tempr;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (tempr < 0)tempr = 32768 - tempr;
+	ctemp = tempr >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = tempr;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(press<0)press=32768-press;
-	ctemp=press>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=press;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (press < 0)press = 32768 - press;
+	ctemp = press >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = press;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	USART_SendData(USART1,temp%256);
-	USART_SendData(USART1,0xaa);
+	USART_SendData(USART1, temp % 256);
+	USART_SendData(USART1, 0xaa);
 }
 
 /**************************实现函数********************************************
@@ -187,203 +187,203 @@ void UART1_ReportIMU(s16 yaw,s16 pitch,s16 roll,s16 alt,s16 tempr,s16 press,s16 
 	int16_t hx  磁罗盘 X轴ADC输出 范围 ：一个有符号整型
 	int16_t hy  磁罗盘 Y轴ADC输出 范围 ：一个有符号整型
 	int16_t hz  磁罗盘 Z轴ADC输出 范围 ：一个有符号整型
-	
-输出参数：没有	
+
+输出参数：没有
 *******************************************************************************/
-void UART1_ReportMotion(s16 ax,s16 ay,s16 az,s16 gx,s16 gy,s16 gz,
-					s16 hx,s16 hy,s16 hz)
+void UART1_ReportMotion(s16 ax, s16 ay, s16 az, s16 gx, s16 gy, s16 gz,
+                        s16 hx, s16 hy, s16 hz)
 {
- 	unsigned int temp=0xaF+9;
+	unsigned int temp = 0xaF + 9;
 	static char ctemp;
-	USART_SendData(USART1,0xa5);
-	USART_SendData(USART1,0x5a);
-	USART_SendData(USART1,14+8);
-	USART_SendData(USART1,0xA2);
+	USART_SendData(USART1, 0xa5);
+	USART_SendData(USART1, 0x5a);
+	USART_SendData(USART1, 14 + 8);
+	USART_SendData(USART1, 0xA2);
 
-	if(ax<0)ax=32768-ax;
-	ctemp=ax>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=ax;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (ax < 0)ax = 32768 - ax;
+	ctemp = ax >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = ax;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(ay<0)ay=32768-ay;
-	ctemp=ay>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=ay;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (ay < 0)ay = 32768 - ay;
+	ctemp = ay >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = ay;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(az<0)az=32768-az;
-	ctemp=az>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=az;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (az < 0)az = 32768 - az;
+	ctemp = az >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = az;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(gx<0)gx=32768-gx;
-	ctemp=gx>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=gx;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (gx < 0)gx = 32768 - gx;
+	ctemp = gx >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = gx;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(gy<0)gy=32768-gy;
-	ctemp=gy>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=gy;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (gy < 0)gy = 32768 - gy;
+	ctemp = gy >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = gy;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 //-------------------------
-	if(gz<0)gz=32768-gz;
-	ctemp=gz>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=gz;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (gz < 0)gz = 32768 - gz;
+	ctemp = gz >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = gz;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(hx<0)hx=32768-hx;
-	ctemp=hx>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=hx;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (hx < 0)hx = 32768 - hx;
+	ctemp = hx >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = hx;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(hy<0)hy=32768-hy;
-	ctemp=hy>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=hy;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (hy < 0)hy = 32768 - hy;
+	ctemp = hy >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = hy;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	if(hz<0)hz=32768-hz;
-	ctemp=hz>>8;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
-	ctemp=hz;
-	USART_SendData(USART1,ctemp);
-	temp+=ctemp;
+	if (hz < 0)hz = 32768 - hz;
+	ctemp = hz >> 8;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
+	ctemp = hz;
+	USART_SendData(USART1, ctemp);
+	temp += ctemp;
 
-	USART_SendData(USART1,temp%256);
-	USART_SendData(USART1,0xaa);
+	USART_SendData(USART1, temp % 256);
+	USART_SendData(USART1, 0xaa);
 }
-void DMA_UART1_ReportMotion(s16 ax,s16 ay,s16 az,s16 gx,s16 gy,s16 gz,
-					s16 hx,s16 hy,s16 hz)
+void DMA_UART1_ReportMotion(s16 ax, s16 ay, s16 az, s16 gx, s16 gy, s16 gz,
+                            s16 hx, s16 hy, s16 hz)
 {
- 	unsigned int temp=0xaF+9;
+	unsigned int temp = 0xaF + 9;
 	static char ctemp;
 	//USART_SendData(USART1,0xa5);
 	//USART_SendData(USART1,0x5a);
 	//USART_SendData(USART1,14+8);
 	//USART_SendData(USART1,0xA2);
-	buf[0]=0xA5;
-	buf[1]=0x5a;
-	buf[2]=22;
-	buf[3]=0xA2;
+	buf[0] = 0xA5;
+	buf[1] = 0x5a;
+	buf[2] = 22;
+	buf[3] = 0xA2;
 
-	if(ax<0)ax=32768-ax;
-	ctemp=ax>>8;
+	if (ax < 0)ax = 32768 - ax;
+	ctemp = ax >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[4]=ctemp;
-	temp+=ctemp;
-	ctemp=ax;
+	buf[4] = ctemp;
+	temp += ctemp;
+	ctemp = ax;
 	//USART_SendData(USART1,ctemp);
-	buf[5]=ctemp;
-	temp+=ctemp;
+	buf[5] = ctemp;
+	temp += ctemp;
 
-	if(ay<0)ay=32768-ay;
-	ctemp=ay>>8;
+	if (ay < 0)ay = 32768 - ay;
+	ctemp = ay >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[6]=ctemp;
-	temp+=ctemp;
-	ctemp=ay;
+	buf[6] = ctemp;
+	temp += ctemp;
+	ctemp = ay;
 	//USART_SendData(USART1,ctemp);
-	buf[7]=ctemp;
-	temp+=ctemp;
+	buf[7] = ctemp;
+	temp += ctemp;
 
-	if(az<0)az=32768-az;
-	ctemp=az>>8;
+	if (az < 0)az = 32768 - az;
+	ctemp = az >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[8]=ctemp;
-	temp+=ctemp;
-	ctemp=az;
+	buf[8] = ctemp;
+	temp += ctemp;
+	ctemp = az;
 	//USART_SendData(USART1,ctemp);
-	buf[9]=ctemp;
-	temp+=ctemp;
+	buf[9] = ctemp;
+	temp += ctemp;
 
-	if(gx<0)gx=32768-gx;
-	ctemp=gx>>8;
+	if (gx < 0)gx = 32768 - gx;
+	ctemp = gx >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[10]=ctemp;
-	temp+=ctemp;
-	ctemp=gx;
+	buf[10] = ctemp;
+	temp += ctemp;
+	ctemp = gx;
 	//USART_SendData(USART1,ctemp);
-	buf[11]=ctemp;
-	temp+=ctemp;
+	buf[11] = ctemp;
+	temp += ctemp;
 
-	if(gy<0)gy=32768-gy;
-	ctemp=gy>>8;
+	if (gy < 0)gy = 32768 - gy;
+	ctemp = gy >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[12]=ctemp;
-	temp+=ctemp;
-	ctemp=gy;
+	buf[12] = ctemp;
+	temp += ctemp;
+	ctemp = gy;
 	//USART_SendData(USART1,ctemp);
-	buf[13]=ctemp;
-	temp+=ctemp;
+	buf[13] = ctemp;
+	temp += ctemp;
 //-------------------------
-	if(gz<0)gz=32768-gz;
-	ctemp=gz>>8;
+	if (gz < 0)gz = 32768 - gz;
+	ctemp = gz >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[14]=ctemp;
-	temp+=ctemp;
-	ctemp=gz;
+	buf[14] = ctemp;
+	temp += ctemp;
+	ctemp = gz;
 	//USART_SendData(USART1,ctemp);
-	buf[15]=ctemp;
-	temp+=ctemp;
+	buf[15] = ctemp;
+	temp += ctemp;
 
-	if(hx<0)hx=32768-hx;
-	ctemp=hx>>8;
+	if (hx < 0)hx = 32768 - hx;
+	ctemp = hx >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[16]=ctemp;
-	temp+=ctemp;
-	ctemp=hx;
+	buf[16] = ctemp;
+	temp += ctemp;
+	ctemp = hx;
 	//USART_SendData(USART1,ctemp);
-	buf[17]=ctemp;
-	temp+=ctemp;
+	buf[17] = ctemp;
+	temp += ctemp;
 
-	if(hy<0)hy=32768-hy;
-	ctemp=hy>>8;
+	if (hy < 0)hy = 32768 - hy;
+	ctemp = hy >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[18]=ctemp;
-	temp+=ctemp;
-	ctemp=hy;
+	buf[18] = ctemp;
+	temp += ctemp;
+	ctemp = hy;
 	//USART_SendData(USART1,ctemp);
-	buf[19]=ctemp;
-	temp+=ctemp;
+	buf[19] = ctemp;
+	temp += ctemp;
 
-	if(hz<0)hz=32768-hz;
-	ctemp=hz>>8;
+	if (hz < 0)hz = 32768 - hz;
+	ctemp = hz >> 8;
 	//USART_SendData(USART1,ctemp);
-	buf[20]=ctemp;
-	temp+=ctemp;
-	ctemp=hz;
+	buf[20] = ctemp;
+	temp += ctemp;
+	ctemp = hz;
 	//USART_SendData(USART1,ctemp);
-	buf[21]=ctemp;
-	temp+=ctemp;
+	buf[21] = ctemp;
+	temp += ctemp;
 
 	//USART_SendData(USART1,temp%256);
 	//USART_SendData(USART1,0xaa);
-	buf[22]=temp%256;
-	buf[23]=0xaa;
-	DMA1_USART1_SEND((u32)buf,24);
+	buf[22] = temp % 256;
+	buf[23] = 0xaa;
+	DMA1_USART1_SEND((u32)buf, 24);
 }
 
 
--- USER/main.c
+++ USER/main.c
@@ -1,40 +1,40 @@
 #include "include.h"//所有头文件全部包含了
 int main(void)
-{	
-	u8 ReadID1=0,ReadID2=0;
-  ALL_INIT();
-  delay_ms(50);
- /* while(1)
+{
+	u8 ReadID1 = 0, ReadID2 = 0;
+	ALL_INIT();
+	delay_ms(50);
+	/* while(1)
 	{
 		Read_HMC5883(&HMC5883_LAST);
 		printf("%d,%d\n",HMC5883_LAST.X,HMC5883_LAST.Y);
 		delay_ms(20);
-  }*/
-  printf("硬件初始化完成\n");	 
-	ReadID1=Single_Read(ADXL345_I2C_ADDR,ADXL345_ID);
-	ReadID2=Single_Read(L3G4200_I2C_ADDR,L3G4200_ID);
-	if(ReadID1==0XE5&&ReadID2==0XD3)  printf("设备ID验证成功\n");
-	else while(1) {  printf("设备硬件验证错误---请检查IIC连线\n"); delay_ms(100);	}
-  printf("陀螺仪的水平校准--请保持设备水平静止至少3秒\n");
-  Calculation_ACC_Offset();
-  printf("陀螺仪静态误差校准完成\n");
-  Read_HMC5883(&HMC5883_LAST);
-  last_yaw=atan2(HMC5883_LAST.X,HMC5883_LAST.Y) + PI;
-  TIM_Cmd(TIM5,ENABLE);//定时器中断使能
-  while(1)
-	{    
-    if(FLAG_Send==1)
-			 {				 
-		   FLAG_Send=0; 
-	     //PTB_OUT(11)=0;
-		   DMA_UART1_ReportIMU(Q_ANGLE.YAW*10,-1*Q_ANGLE.ROLL*10,-1*Q_ANGLE.PITCH*10,0,0,0,0);
-			 //DMA_UART1_ReportIMU(Q_ANGLE.PITCH*10,-1*angle_ax*10,0,0,0,0,0);
+	 }*/
+	printf("硬件初始化完成\n");
+	ReadID1 = Single_Read(ADXL345_I2C_ADDR, ADXL345_ID);
+	ReadID2 = Single_Read(L3G4200_I2C_ADDR, L3G4200_ID);
+	if (ReadID1 == 0XE5 && ReadID2 == 0XD3)  printf("设备ID验证成功\n");
+	else while (1) {  printf("设备硬件验证错误---请检查IIC连线\n"); delay_ms(100);	}
+	printf("陀螺仪的水平校准--请保持设备水平静止至少3秒\n");
+	Calculation_ACC_Offset();
+	printf("陀螺仪静态误差校准完成\n");
+	Read_HMC5883(&HMC5883_LAST);
+	last_yaw = atan2(HMC5883_LAST.X, HMC5883_LAST.Y) + PI;
+	TIM_Cmd(TIM5, ENABLE); //定时器中断使能
+	while (1)
+	{
+		if (FLAG_Send == 1)
+		{
+			FLAG_Send = 0;
+			//PTB_OUT(11)=0;
+			DMA_UART1_ReportIMU(Q_ANGLE.YAW * 10, -1 * Q_ANGLE.ROLL * 10, -1 * Q_ANGLE.PITCH * 10, 0, 0, 0, 0);
+			//DMA_UART1_ReportIMU(Q_ANGLE.PITCH*10,-1*angle_ax*10,0,0,0,0,0);
 			//DMA_UART1_ReportMotion(ACC_AVG.X,ACC_AVG.Y,ACC_AVG.Z,L3G4200_GYRO_LAST.X,L3G4200_GYRO_LAST.Y,L3G4200_GYRO_LAST.Z,HMC5883_LAST.X,HMC5883_LAST.Y,HMC5883_LAST.Z);
-		   //DMA_UART1_ReportMotion(Q_ANGLE.PITCH*20,-1*angle_ax*20,0,0,0,0,0,0,0); //ax   ay
-			 //PTB_OUT(11)=1;
-			 }
-			 
+			//DMA_UART1_ReportMotion(Q_ANGLE.PITCH*20,-1*angle_ax*20,0,0,0,0,0,0,0); //ax   ay
+			//PTB_OUT(11)=1;
+		}
+
 	}
- }
- 
+}
+
 
